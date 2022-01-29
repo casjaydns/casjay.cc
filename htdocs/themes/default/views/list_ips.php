66ad087d55c03ad9b64d7e4a194c954c9026356f
Jason <git-admin@casjaysdev.com>
2022-01-29 04:05:09 -0500

    🗃️ modified: htdocs/themes/default/views/list_ips.php 🗃️

--- htdocs/themes/default/views/list_ips.php
+++ htdocs/themes/default/views/list_ips.php
@@ -3,11 +3,12 @@
 <p><?php echo $total_spam_attempts; ?> spam-pastes repelled. <a href="<?php echo site_url('spamadmin/blacklist'); ?>">View blocked IPs</a>.</p>
 
 		<?php
-		function checkNum($num){
-			return ($num%2) ? TRUE : FALSE;
-		}
-		$n = 0;
-		if(!empty($pastes)){ ?>
+function checkNum($num)
+{
+    return ($num % 2) ? true : false;
+}
+$n = 0;
+if (!empty($pastes)) {?>
 			<table class="recent selectable">
 				<tbody>
 					<tr>
@@ -17,19 +18,20 @@
 						<th class="time">IP</th>
 						<th title="Quick remove" class="hidden">X</th>
 					</tr>
-		<?php	foreach($pastes as $paste) {
-				if(checkNum($n) == TRUE) {
-					$eo = "even";
-				} else {
-					$eo = "odd";
-				}
-				$n++;
-		?>
+		<?php	foreach ($pastes as $paste) {
+    if (checkNum($n) == true) {
+        $eo = "even";
+    } else {
+        $eo = "odd";
+    }
+    $n++;
+    ?>
 
 		<tr class="<?php echo $eo; ?>">
-			<td class="first"><a href="<?php echo site_url("view/".$paste['pid']); ?>"><?php echo $paste['title']; ?></a></td>
+			<td class="first"><a href="<?php echo site_url("view/" . $paste['pid']); ?>"><?php echo $paste['title']; ?></a></td>
 			<td><?php echo $paste['name']; ?></td>
-			<td><?php $p = explode(",", timespan($paste['created'], time())); echo $p[0]; ?> ago.</td>
+			<td><?php $p = explode(",", timespan($paste['created'], time()));
+    echo $p[0];?> ago.</td>
 			<td><a href="<?php echo site_url('spamadmin/' . $paste['ip_address']) ?>"><?php echo $paste['ip_address']; ?></a></td>
 			<td class="hidden"><a class="quick_remove" title="Quickly remove all entries with this IP" data-ip="<?php echo $paste['ip_address']; ?>" href="">X</a></td>
 		</tr>
@@ -46,7 +48,7 @@
                 <input type="submit" name="delete_pastes" value="Delete selected pastes" class="inv" />
             </form>
 
-		<?php } else { ?>
+		<?php } else {?>
 			<p><?php echo lang('paste_missing'); ?> :(</p>
 		<?php }?>
 <?php echo $pages; ?>
