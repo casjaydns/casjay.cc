1b3a2a880c86cf13007d28809939f8b8e7bd1e86
casjay <git-admin@casjaysdev.pro>
2024-09-20 08:23:01 -0400

    🗃️ Committing everything that changed 🗃️
    htdocs/themes/default/views/defaults/footer_message.php

--- htdocs/themes/default/views/defaults/footer_message.php
+++ htdocs/themes/default/views/defaults/footer_message.php
@@ -1,104 +1,110 @@
 <!-- Begin Footer -->
 <div id="footer" class="footer" align="center">
-<br /><br /><br /><br /><br /><br /><br />
-<!-- Begin Casjays Developments Footer -->
-    Copyright 1999 - 2023
-    <br />
-    <a href="https://casjay.pro" target="_blank">Casjays Developments</a> and <a href="https://jason.malaks.us" target="_blank">Jason M. Hempstead-Malak</a><br />
-    Hosting by <a href="https://casjay.pro/hosting" target="_blank">Casjays Developments: Hosting</a><br />
-    and powered by <a href="https://casjaysdev.pro" target="_blank">CasjaysDev.com</a>
-    <br />
-    <br />
-    <a href="https://www.patreon.com/casjay" target="_blank"><img src="https://img.shields.io/badge/patreon-donate-orange.svg" border="0" alt="Casjays Patreon Page"></a>
-    <a href="https://www.paypal.me/casjaysdev" target="_blank"><img src="https://img.shields.io/badge/Donate-PayPal-green.svg?casjay@yahoo.com" alt="Casjays Paypal Page"></a>
-    <br />
-    <br />
-    <a href="https://help.casjay.pro" target="_blank">CasjaysDev Support</a> |
-    <a href="https://bugs.casjay.pro" target="_blank">CasjaysDev Bugs</a> <br />
-    <a href="https://status.casjay.pro" target="_blank">System Status</a> |
-    <a href="https://servers.casjay.pro" target="_blank">Service Status</a> |
-    <a href="https://status.casjaysdev.pro" target="_blank">Complete Status</a> <br />
-    <a href="https://casjaysdev.pro/domains.html" target="_blank">Casjays Developments Domains</a>
-    <br />
-    <br />
-    Casjays Developments privacy policy can be found at<br />
-    <a href="https://casjaysdev.pro/policy/" target="_blank">https://casjaysdev.pro/policy</a>
-    <br />
-    <!-- End Casjays Developments Footer -->
+  <br /><br /><br /><br /><br /><br /><br />
+  <!-- Begin Casjays Developments Footer -->
+  Copyright 1999 - 2023
+  <br />
+  <a href="https://casjay.pro" target="_blank">Casjays Developments</a> and <a href="https://jason.malaks.us"
+    target="_blank">Jason M. Hempstead-Malak</a><br />
+  Hosting by <a href="https://casjay.pro/hosting" target="_blank">Casjays Developments: Hosting</a><br />
+  and powered by <a href="https://casjaysdev.pro" target="_blank">CasjaysDev.pro</a>
+  <br />
+  <br />
+  <a href="https://www.patreon.com/casjay" target="_blank"><img
+      src="https://img.shields.io/badge/patreon-donate-orange.svg" border="0" alt="Casjays Patreon Page"></a>
+  <a href="https://www.paypal.me/casjaysdev" target="_blank"><img
+      src="https://img.shields.io/badge/Donate-PayPal-green.svg?casjay@yahoo.com" alt="Casjays Paypal Page"></a>
+  <br />
+  <br />
+  <a href="https://help.casjay.pro" target="_blank">CasjaysDev Support</a> |
+  <a href="https://bugs.casjay.pro" target="_blank">CasjaysDev Bugs</a> <br />
+  <a href="https://status.casjay.pro" target="_blank">System Status</a> |
+  <a href="https://servers.casjay.pro" target="_blank">Service Status</a> |
+  <a href="https://status.casjaysdev.pro" target="_blank">Complete Status</a> <br />
+  <a href="https://casjaysdev.pro/domains.html" target="_blank">Casjays Developments Domains</a>
+  <br />
+  <br />
+  Casjays Developments privacy policy can be found at<br />
+  <a href="https://casjaysdev.pro/policy/" target="_blank">https://casjaysdev.pro/policy</a>
+  <br />
+  <!-- End Casjays Developments Footer -->
 
-    <!-- Begin theme Code -->
-    <!-- Theme powered -->
-    <div class="text-center" style="color: royalblue;">
-      <br />
-      Made with 💜💜💜💜💜💜 by <br />
-      <a href="http://github.com/casjay" target="_blank" rel="noopener">
-        Jason M. Hempstead (Casjay)</a><br />
-      This site is powered by opensource software<br />
-      A <a href="https://github.com/casjay-coffee/casjay.cc" target="_blank" rel="noopener">customized</a> version of <a href="https://github.com/claudehohl/Stikked" target="_blank" rel="noopener">Stikked</a>
-    </div>
+  <!-- Begin theme Code -->
+  <!-- Theme powered -->
+  <div class="text-center" style="color: royalblue;">
     <br />
-    <!-- End Theme powered -->
-
-    <!-- End theme Code -->
-    <!-- Begin last updated -->
-    <span style="color: skyblue;">
-      Last update on: Feb 20, 2023 at 15:25
-    </span>
-    <!-- End last updated -->
-    <!--Begin Statcounter Code -->
-
-    <!-- End Statcounter Code -->
-    <!-- Begin Google Track -->
-
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-936146-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-      gtag('config', 'UA-936146-1');
-    </script>
-
-    <!-- Eng Google Track -->
-    <!--Begin ads Code -->
-
-    <br />
-
-    <!-- End ads Code -->
-<!-- Begin EU compliant -->
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
-<script>
-window.addEventListener("load", function(){
-window.cookieconsent.initialise({
-  "palette": {
-    "popup": {
-      "background": "#64386b",
-      "text": "#ffcdfd"
-    },
-    "button": {
-      "background": "transparent",
-      "text": "#f8a8ff",
-      "border": "#f8a8ff"
-    }
-  },
-  "content": {
-    "message": "In accordance with the EU GDPR law this message is being displayed. - ",
-    "dismiss": "I Agree",
-    "link": "CasjaysDev Policy",
-    "href": "https://casjaysdev.pro/policy/"
-  },
-  "type": "opt-out"
-})});
-</script>
-<!-- End EU compliant -->
-<br />
-<br />
-<br />
-<br />
-<br />
-
+    Made with 💜💜💜💜💜💜 by <br />
+    <a href="http://github.com/casjay" target="_blank" rel="noopener">
+      Jason M. Hempstead (Casjay)</a><br />
+    This site is powered by opensource software<br />
+    A <a href="https://github.com/casjay-coffee/casjay.cc" target="_blank" rel="noopener">customized</a> version of <a
+      href="https://github.com/claudehohl/Stikked" target="_blank" rel="noopener">Stikked</a>
   </div>
+  <br />
+  <!-- End Theme powered -->
+
+  <!-- End theme Code -->
+  <!-- Begin last updated -->
+  <span style="color: skyblue;">
+    Last update on: Feb 20, 2023 at 15:25
+  </span>
+  <!-- End last updated -->
+  <!--Begin Statcounter Code -->
+
+  <!-- End Statcounter Code -->
+  <!-- Begin Google Track -->
+
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-936146-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'UA-936146-1');
+  </script>
+
+  <!-- Eng Google Track -->
+  <!--Begin ads Code -->
+
+  <br />
+
+  <!-- End ads Code -->
+  <!-- Begin EU compliant -->
+  <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+  <script>
+    window.addEventListener("load", function () {
+      window.cookieconsent.initialise({
+        "palette": {
+          "popup": {
+            "background": "#64386b",
+            "text": "#ffcdfd"
+          },
+          "button": {
+            "background": "transparent",
+            "text": "#f8a8ff",
+            "border": "#f8a8ff"
+          }
+        },
+        "content": {
+          "message": "In accordance with the EU GDPR law this message is being displayed. - ",
+          "dismiss": "I Agree",
+          "link": "CasjaysDev Policy",
+          "href": "https://casjaysdev.pro/policy/"
+        },
+        "type": "opt-out"
+      })
+    });
+  </script>
+  <!-- End EU compliant -->
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+
+</div>
 </div>
