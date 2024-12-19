99b8d8c208f7ca7f4c766824cd6a6709cd3eab22
casjay <git-admin@casjaysdev.pro>
2024-12-19 13:27:02 -0500

    🗃️ Committing everything that changed 🗃️
    htdocs/themes/default/views/defaults/header.php

--- htdocs/themes/default/views/defaults/header.php
+++ htdocs/themes/default/views/defaults/header.php
@@ -12,6 +12,7 @@ $theme = $this->config->item('theme');
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 
 	<?php
