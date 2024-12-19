c01967541ae9497b8dad24e3e9379247e7e45d5e
casjay <git-admin@casjaysdev.pro>
2024-12-19 13:29:58 -0500

    🗃️ Committing everything that changed 🗃️
    htdocs/themes/default/views/defaults/header.php

--- htdocs/themes/default/views/defaults/header.php
+++ htdocs/themes/default/views/defaults/header.php
@@ -14,6 +14,7 @@ $theme = $this->config->item('theme');
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/5/darkly/bootstrap.css">
 
 	<?php
 	//Carabiner
@@ -47,7 +48,6 @@ $theme = $this->config->item('theme');
 
 	<link rel="stylesheet" type="text/css"
 		href="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css">
-	<link rel="stylesheet" type="text/css" href="https://bootswatch.com/5/darkly/bootstrap.css">
 	<title><?php echo $page_title; ?></title>
 </head>
 
